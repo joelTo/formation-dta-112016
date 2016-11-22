bba733dd28772e51eccf8cd63d62e206e76c77db
Fawzi NASSIM <fawzi.nassim@gmail.com>
2016-11-22 17:27:11 +0100

    Add Pizza

--- pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -3,17 +3,74 @@ package fr.pizzeria.console;
 import java.util.Scanner;
 
 public class PizzeriaAdminConsoleApp {
+	
+	public static String[] menu = new String[6];
+	public static Scanner reader = new Scanner(System.in);  // Reading from System.in.
+	public static String[][] pizzas = {{"0", "PEP", "Pépéroni", "12.50"},
+			{"1", "MAR", "Margherita", "14.00"},
+			{"2", "REI", "La Reine", "11.50"},
+			{"3", "FRO", "La 4 fromages", "12.00"},
+			{"4", "CAN", "La cannibale", "12.50"},
+			{"5", "SAV", "La savoyarde", "13.00"},
+			{"6", "ORI", "L’orientale", "13.50"},
+			{"7", "IND", "L’indienne", "14.00"},
+	};
 
+	
+	public static void pushPizzaToArray(String[] newPizza) {
+
+		String[][] newMenu = new String[pizzas.length + 1][4];
+		
+		for (int i = 0; i < pizzas.length; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				newMenu[i][j] = pizzas[i][j];
+			}
+		}
+		
+		newMenu[pizzas.length][0] = Integer.toString(pizzas.length);
+		newMenu[pizzas.length][1] = newPizza[0];
+		newMenu[pizzas.length][2] = newPizza[1];
+		newMenu[pizzas.length][3] = newPizza[2];
+		
+		pizzas = newMenu;
+	}
+	
 	public static void printPizzaList() {
-		System.out.println("Liste des pizzas");
+
+		for (String[] p: pizzas)
+		{
+			System.out.println(p[1] + " -> " + p[2] + " (" + p[3] + " €)");
+		}
 	}
 
 	public static void createPizza() {
-		System.out.println("Ajout d’une nouvelle pizza");
+
+		System.out.println("Veuillez saisir le code");
+		String code = reader.next(); // Scans the next token of the input as an int.
+
+		System.out.println("Veuillez saisir le nom (sans espace)");
+		String name = reader.next(); // Scans the next token of the input as an int.
+		
+		System.out.println("Veuillez saisir le prix");
+		String price = reader.next(); // Scans the next token of the input as an int.
+		
+		String[] newPizza = {code, name, price};
+		pushPizzaToArray(newPizza);
 	}
 
 	public static void updatePizza() {
-		System.out.println("Mise à jour d’une pizza");
+
+		System.out.println("Veuillez saisir le code");
+		String code = reader.next(); // Scans the next token of the input as an int.
+
+		System.out.println("Veuillez saisir le nom (sans espace)");
+		String name = reader.next(); // Scans the next token of the input as an int.
+		
+		System.out.println("Veuillez saisir le prix");
+		String price = reader.next(); // Scans the next token of the input as an int.
+		
+		String[] newPizza = {code, name, price};
+		System.out.println(newPizza[2]);
 	}	
 
 	public static void deletePizza() {
@@ -24,9 +81,7 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("Aurevoir :(");
 	}
 
-	public static void printMenu() {
-
-		String[] menu = new String[6];
+	public static void printMenu(String[][] pizzas) {
 
 		menu[0] = "***** Pizzeria Administration *****";
 		menu[1] = "1. Lister les pizzas";
@@ -34,7 +89,7 @@ public class PizzeriaAdminConsoleApp {
 		menu[3] = "3. Mettre à jour une pizza";
 		menu[4] = "4. Supprimer une pizza";
 		menu[5] = "99. Sortir";
-		
+
 		boolean flag = false;
 
 		while (true) {
@@ -43,7 +98,6 @@ public class PizzeriaAdminConsoleApp {
 				System.out.println(s);
 			}
 
-			Scanner reader = new Scanner(System.in);  // Reading from System.in.
 			int choice = reader.nextInt(); // Scans the next token of the input as an int.
 
 			switch (choice) {
@@ -65,12 +119,13 @@ public class PizzeriaAdminConsoleApp {
 				break;
 			}
 			if (flag) {
+				reader.close();
 				break;
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		printMenu();
+		printMenu(pizzas);
 	}
 }
